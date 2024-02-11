0d73a57ccc56e7c9483a7ab061549985ee3e57a3
Genilson fernandes <geniilsonfernandes@gmail.com>
2024-02-11 11:30:35 -0300

    feat: create brand logo

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,17 @@
+import { StoryObj } from '@storybook/react'
+
+import Logo from '.'
+
+const meta = {
+  title: 'Components/Logo',
+  component: Logo,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof Logo>
+
+export const Default: Story = {
+  args: {},
+}
---
+++ src/components/Logo/Logo.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+
+import Logo from '.'
+
+describe('Logo', () => {
+  it('should render Logo', () => {
+    render(<Logo />)
+
+    expect(screen.getByLabelText("just travel's brand")).toBeTruthy()
+  })
+})
