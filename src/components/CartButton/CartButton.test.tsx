8c8a025141692bac23494741d5ae2f6f6c5cd003
Genilson fernandes <geniilsonfernandes@gmail.com>
2024-02-11 11:29:59 -0300

    fix: add event to open modal on click cart button

--- src/components/CartButton/CartButton.test.tsx
+++ src/components/CartButton/CartButton.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest'
 
 import CartButton from '.'
@@ -8,5 +8,15 @@ describe('CartButton', () => {
     render(<CartButton />)
 
     expect(screen.getByRole('button', { name: 'Cart button' })).toBeTruthy()
+  })
+  it('should open cart when button is clicked and close', async () => {
+    render(<CartButton />)
+    const cartButton = screen.getByRole('button', { name: 'Cart button' })
+    fireEvent.click(cartButton)
+    expect(screen.getByRole('dialog', { name: 'Cart' })).toBeTruthy()
+
+    fireEvent.click(cartButton)
+
+    expect(screen.queryByRole('dialog', { name: 'Cart' })).toBeNull()
   })
 })
--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,20 +1,56 @@
+import useClickOutside from '@/hooks/useClickOutside'
 import { ShoppingCartSimple } from '@phosphor-icons/react'
+import { useState } from 'react'
+import Typography from '../Typography'
+
+// type ClikOutsideProps = React.HTMLAttributes<HTMLDivElement>
+// const ClikOutside = (props: ClikOutsideProps) => (
+//   <div className='fixed top-0 left-0 w-full h-full ' {...props} />
+// )
 
 const CartButton = () => {
+  const cartItems = 5
+
+  const [show, setShow] = useState(false)
+
+  const handleTogle = () => {
+    setShow(!show)
+  }
+
+  const ref = useClickOutside(show, handleTogle)
+
   return (
-    <button
-      className='h-[50px] px-3 bg-primary-100 rounded flex items-center gap-2 hover:bg-primary-200 transition-colors duration-300'
-      aria-label='Cart button'
-      name='Cart button'
-    >
-      <ShoppingCartSimple size={20} weight='fill' />
-      <div
-        className='h-[32px] w-[32px]	bg-white/25 rounded-full flex items-center justify-center'
-        aria-label='Cart quantity'
+    <div className='relative'>
+      <button
+        className='h-[50px] px-2 pl-3 bg-primary-100 rounded flex items-center gap-[6px] hover:bg-primary-200 transition-colors duration-300'
+        aria-label='Cart button'
+        name='Cart button'
+        onClick={handleTogle}
       >
-        0
-      </div>
-    </button>
+        <span className='px-1'>
+          <ShoppingCartSimple size={20} weight='fill' />
+        </span>
+        <div
+          className='h-[32px] min-w-[32px] px-1 bg-white/25 rounded-full flex items-center justify-center font-medium'
+          aria-label='Cart quantity'
+        >
+          {cartItems}
+        </div>
+      </button>
+      {/* {show && <ClikOutside onClick={handleTogle} />} */}
+      {show && (
+        <div
+          className='absolute w-[494px] h-[250px] bg-white shadow-lg right-0 top-[80px] animate-fadeIn'
+          ref={ref}
+          aria-label='Cart'
+          role='dialog'
+        >
+          <Typography as='span' className='text-white'>
+            cart
+          </Typography>
+        </div>
+      )}
+    </div>
   )
 }
 
