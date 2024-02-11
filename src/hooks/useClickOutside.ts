deca78f8eab37311fe956a39f88f757db9da7926
Genilson fernandes <geniilsonfernandes@gmail.com>
2024-02-11 11:31:57 -0300

    feat: add news hooks and utils to project

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react'
+
+const useClickOutside = (isOpen: boolean, callback: () => void) => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      callback()
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      console.log('isOpen', isOpen)
+
+      document.addEventListener('click', handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [isOpen])
+
+  return ref
+}
+
+export default useClickOutside
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount = 0) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(amount)
+}
