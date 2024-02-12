8d193557c56897209058a3657c05d0242b47d685
Genilson fernandes <geniilsonfernandes@gmail.com>
2024-02-12 14:56:53 -0300

    add type case in test

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -1,15 +1,19 @@
-import { cleanup, render, screen } from '@testing-library/react'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 import { afterEach, describe, expect, it } from 'vitest'
 
 import Search from '.'
 
 afterEach(cleanup)
 
-describe('Search', () => {
-  it('should render Search', async () => {
+describe('Search rendering', () => {
+  it('should render Search and  type in input', async () => {
     render(<Search />)
     const input = screen.getByPlaceholderText('Busque por atração')
 
-    expect(input).toBeTruthy()
+    fireEvent.change(input, {
+      target: { value: 'foo' },
+    })
+
+    expect(input).toHaveValue('foo')
   })
 })
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,20 +2,22 @@ import { MagnifyingGlass, MapPin } from '@phosphor-icons/react/dist/ssr'
 
 const Search = () => {
   return (
-    <div className='container py-9'>
-      <div className='border border-gray-10 h-12  flex items-center'>
-        <button className='h-full w-12 flex items-center justify-center'>
-          <MapPin size={21} className='text-brand-color-blue' />
-        </button>
-        <input
-          type='text'
-          name='Search input'
-          placeholder='Busque por atração'
-          className='w-full h-full placeholder:text-gray-300 placeholder:text-sm bg-transparent outline-none text-gray-500'
-        />
-        <button className='h-full w-12 flex items-center justify-center border-l border-gray-10 hover:bg-gray-10'>
-          <MagnifyingGlass size={21} className='text-gray-300 ' />
-        </button>
+    <div className='py-9 '>
+      <div className='container'>
+        <div className='border border-gray-10 h-12  flex items-center'>
+          <button className='h-full w-12 flex items-center justify-center'>
+            <MapPin size={21} className='text-brand-color-blue' />
+          </button>
+          <input
+            type='text'
+            name='Search input'
+            placeholder='Busque por atração'
+            className='w-full h-full placeholder:text-gray-300 placeholder:text-sm bg-transparent outline-none text-gray-500'
+          />
+          <button className='h-full w-12 flex items-center justify-center border-l border-gray-10 hover:bg-gray-10'>
+            <MagnifyingGlass size={21} className='text-gray-300 ' />
+          </button>
+        </div>
       </div>
     </div>
   )
