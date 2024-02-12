8ea57c942b326c5d30661d8fa0471ed50ece6319
Genilson fernandes <geniilsonfernandes@gmail.com>
2024-02-12 14:49:21 -0300

    feat: create icons

---
+++ src/components/ui/Icons/index.tsx
@@ -0,0 +1,274 @@
+const Star = () => (
+  <svg
+    width='20'
+    height='20'
+    viewBox='0 0 20 20'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M10 15.2351L4.179 18.0001L5.388 11.8801L1 7.39206L7.179 6.62106L10 1.00006L12.821 6.62106L19 7.39206L14.612 11.8801L15.821 18.0001L10 15.2351Z'
+      fill='#FFAD0D'
+      stroke='#FFAD0D'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
+const Check = () => (
+  <svg
+    width='12'
+    height='10'
+    viewBox='0 0 12 10'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M10.267 0.667845C10.4596 0.484427 10.716 0.383026 10.982 0.385039C11.248 0.387052 11.5028 0.492321 11.6927 0.678633C11.8826 0.864945 11.9926 1.11773 11.9996 1.38364C12.0067 1.64955 11.9101 1.9078 11.7304 2.10388L6.27343 8.92849C6.1796 9.02956 6.06635 9.11066 5.94045 9.16696C5.81456 9.22326 5.6786 9.25359 5.54071 9.25615C5.40283 9.2587 5.26584 9.23342 5.13794 9.18182C5.01005 9.13022 4.89387 9.05336 4.79636 8.95584L1.17755 5.33702C1.07677 5.24312 0.995936 5.12987 0.939873 5.00405C0.88381 4.87823 0.853664 4.7424 0.851234 4.60467C0.848804 4.46695 0.874139 4.33014 0.925729 4.20242C0.977318 4.0747 1.0541 3.95867 1.15151 3.86127C1.24891 3.76387 1.36493 3.68708 1.49266 3.63549C1.62038 3.5839 1.75719 3.55857 1.89491 3.561C2.03264 3.56343 2.16847 3.59357 2.29429 3.64964C2.42011 3.7057 2.53335 3.78653 2.62726 3.88731L5.49113 6.74981L10.241 0.697934L10.267 0.667845Z'
+      fill='currentColor'
+    />
+  </svg>
+)
+
+const Home = () => (
+  <svg
+    width='16'
+    height='16'
+    viewBox='0 0 16 16'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M1.25 7.03562L8 1.25012L14.75 7.03562'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M2.2146 6.20912V14.7501H13.7856V1.25012H10.8929V3.72962'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M6.07178 14.7501V11.4283C6.07178 10.5996 6.74303 9.92834 7.57178 9.92834H8.42903C9.25778 9.92834 9.92903 10.5996 9.92903 11.4283V14.7501'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
+
+const Apartment = () => (
+  <svg
+    width='18'
+    height='18'
+    viewBox='0 0 18 18'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M2.99763 2.99762H10.5008C10.9151 2.99762 11.2511 3.33355 11.2511 3.74793V15.0026H2.99763C2.58324 15.0026 2.24731 14.6667 2.24731 14.2523V3.74793C2.24731 3.33355 2.58324 2.99762 2.99763 2.99762Z'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M11.251 5.9989H15.0025C15.4169 5.9989 15.7529 6.33483 15.7529 6.74921V14.2523C15.7529 14.6667 15.4169 15.0027 15.0025 15.0027H11.251V5.9989Z'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M5.24845 5.90509C5.19665 5.90509 5.15466 5.94708 5.15466 5.99887C5.15466 6.05067 5.19665 6.09266 5.24845 6.09266C5.30025 6.09266 5.34224 6.05067 5.34224 5.99887C5.34224 5.94708 5.30025 5.90509 5.24845 5.90509'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M8.24967 5.90509C8.19787 5.90509 8.15588 5.94708 8.15588 5.99887C8.15588 6.05067 8.19787 6.09266 8.24967 6.09266C8.30147 6.09266 8.34346 6.05067 8.34346 5.99887C8.34346 5.94708 8.30147 5.90509 8.24967 5.90509'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M5.24845 8.90637C5.19665 8.90637 5.15466 8.94836 5.15466 9.00016C5.15466 9.05195 5.19665 9.09394 5.24845 9.09394C5.30025 9.09394 5.34224 9.05195 5.34224 9.00016C5.34224 8.94836 5.30025 8.90637 5.24845 8.90637'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M8.24967 8.90637C8.19787 8.90637 8.15588 8.94836 8.15588 9.00016C8.15588 9.05195 8.19787 9.09394 8.24967 9.09394C8.30147 9.09394 8.34346 9.05195 8.34346 9.00016C8.34346 8.94836 8.30147 8.90637 8.24967 8.90637'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M5.24845 11.9076C5.19665 11.9076 5.15466 11.9496 5.15466 12.0014C5.15466 12.0532 5.19665 12.0952 5.24845 12.0952C5.30025 12.0952 5.34224 12.0532 5.34224 12.0014C5.34224 11.9496 5.30025 11.9076 5.24845 11.9076'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M8.24967 11.9076C8.19787 11.9076 8.15588 11.9496 8.15588 12.0014C8.15588 12.0532 8.19787 12.0952 8.24967 12.0952C8.30147 12.0952 8.34346 12.0532 8.34346 12.0014C8.34346 11.9496 8.30147 11.9076 8.24967 11.9076'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M13.5019 8.90637C13.4501 8.90637 13.4081 8.94836 13.4081 9.00016C13.4081 9.05195 13.4501 9.09394 13.5019 9.09394C13.5537 9.09394 13.5957 9.05195 13.5957 9.00016C13.5957 8.94836 13.5537 8.90637 13.5019 8.90637'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M13.5019 11.9076C13.4501 11.9076 13.4081 11.9496 13.4081 12.0014C13.4081 12.0532 13.4501 12.0952 13.5019 12.0952C13.5537 12.0952 13.5957 12.0532 13.5957 12.0014C13.5957 11.9496 13.5537 11.9076 13.5019 11.9076'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
+const Hotel = () => (
+  <svg
+    width='18'
+    height='18'
+    viewBox='0 0 18 18'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M5.99878 15.7529V2.99763C5.99878 2.58324 6.33471 2.24731 6.74909 2.24731H11.251C11.6654 2.24731 12.0013 2.58324 12.0013 2.99763V15.7529'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M15.7528 15.753V5.93363C15.7528 5.5553 15.4461 5.2486 15.0677 5.2486H12.0012'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M5.99888 5.2486H2.93235C2.55402 5.2486 2.24731 5.5553 2.24731 5.93363V15.753'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M12.0012 7.49955H13.877'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M12.0012 9.75046H13.877'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M12.0012 12.0014H13.877'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M4.12305 7.49955H5.99883'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M4.12305 9.75046H5.99883'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M4.12305 12.0014H5.99883'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M7.87451 9.75046H10.1254'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M7.87451 12.0014H10.1254'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M7.87451 7.49955H10.1254'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M7.87451 5.24857H10.1254'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M9.00016 14.2523V15.7529'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M16.5033 15.753H1.49707'
+      stroke='currentColor'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
+
+export default {
+  Star,
+  Home,
+  Hotel,
+  Apartment,
+  Check,
+}
